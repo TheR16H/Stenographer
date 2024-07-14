ec2a2bb084feeec91822b8486352e91a85b28439
TheR16H <rashawn5hall@gmail.com>
2024-07-14 19:55:23 -0400

    api routes

---
+++ Develop/api routes/notes(api).js
@@ -0,0 +1,2 @@
+// * `DELETE /api/notes/:id
+// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,6 +2,4 @@
 
 // The following API routes should be created:
 
-// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
 
-// * `DELETE /api/notes/:id
