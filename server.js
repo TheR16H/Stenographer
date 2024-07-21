9096f32dbdaee22a3e555440cbd4a9b8bc800c98
TheR16H <rashawn5hall@gmail.com>
2024-07-21 19:08:48 -0400

    set up dependents

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-
-
+const express = require("express");
+const fs = reqiuire("fs");
+const PORT = 3001;
+let db = require('./db/db.json');
+const path = require('path');
 // You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 
