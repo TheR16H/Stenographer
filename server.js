f0068d83ffc68c81923c3b6d062c52f527d1e500
TheR16H <rashawn5hall@gmail.com>
2024-07-21 20:54:03 -0400

    finished dependents

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ let db = require('./db/db.json');
 
 const path = require('path');
 const app = express();
-const 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
 
 
 
