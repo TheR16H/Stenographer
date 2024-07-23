cf5a519830baa8e289402b530979aa8d093e449b
TheR16H <rashawn5hall@gmail.com>
2024-07-23 11:24:15 -0400

    trouble shooting

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Import the routes from notes.js
 const notesRoutes = require('./apiRoutes/notes');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', notesRoutes);
